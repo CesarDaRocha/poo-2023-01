aec7c7c2b22ef4b62106a8de904b2887d97f67a9
CesarDaRocha <cesartapuio@gmail.com>
2023-05-31 20:43:23 -0300

    a

---
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Texto.java
@@ -0,0 +1,17 @@
+package com.github.cesardarocha.poo.t08;
+import java.util.TreeSet;
+
+import jakarta.websocket.Decoder.Text;
+
+public class Texto {
+    private TreeSet paragrafos = new TreeSet<>();
+     public void novoParagrafo(Paragrafo c){
+        paragrafos.add(c);
+
+     }
+
+     public Texto (Paragrafo c){
+        paragrafos.add(c);
+     }
+    
+}
