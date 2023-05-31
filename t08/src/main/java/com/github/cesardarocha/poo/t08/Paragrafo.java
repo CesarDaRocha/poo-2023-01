96156a638bfcbd894d8d1bd8dded0bd09400d632
CesarDaRocha <cesartapuio@gmail.com>
2023-05-31 20:51:32 -0300

    a

--- t08/src/main/java/com/github/cesardarocha/poo/t08/Paragrafo.java
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Paragrafo.java
@@ -1,5 +1,15 @@
 package com.github.cesardarocha.poo.t08;
+import java.util.TreeSet;
 
 public class Paragrafo {
-    private String texto;
+    private TreeSet sentencas = new TreeSet<>();
+     public void novaSentenca(Sentenca c){
+        sentencas.add(c);
+
+     }
+
+     public Paragrafo (Sentenca c){
+        sentencas.add(c);
+     }
+    
 }
