8a3d5cbc7d12c54f074df6736cf3f4f2fd1d0def
CesarDaRocha <cesartapuio@gmail.com>
2023-06-01 19:28:29 -0300

    a

--- t08/src/main/java/com/github/cesardarocha/poo/t08/Pais.java
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Pais.java
@@ -6,16 +6,7 @@ public class Pais {
 
     // Construtor do País
 
-    public Pais() {
-        cidade = new Cidade();
-        cidade.setPaís(this);
-    }
 
-    // Métodos de acesso para a Cidade
-
-    public Cidade getCidade() {
-        return cidade;
-    }
 
     public void setCidade(Cidade cidade) {
         this.cidade = cidade;
