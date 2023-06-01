cbca4ee0980bea0813379485c83fc15d75f32ef0
CesarDaRocha <cesartapuio@gmail.com>
2023-06-01 19:05:03 -0300

    A

---
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Pais.java
@@ -0,0 +1,12 @@
+package com.github.cesardarocha.poo.t08;
+import java.util.*;
+//Pais = Pessoa
+// Cidade = Telefone
+
+public class Pais {
+    private String nome;
+    private Set cidades = new HashSet();
+
+
+}
+
