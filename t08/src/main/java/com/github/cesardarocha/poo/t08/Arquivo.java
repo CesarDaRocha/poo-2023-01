87b47225687882e1ab686d318e383513a25848b6
CesarDaRocha <cesartapuio@gmail.com>
2023-06-01 21:37:13 -0300

    t08

---
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Arquivo.java
@@ -0,0 +1,13 @@
+package com.github.cesardarocha.poo.t08;
+
+public class Arquivo {
+
+//cidade = permissao
+//pais = arquivo
+    
+private Permissao permissao;
+
+    public void setPermissao(Permissao permissao) {
+        this.permissao = permissao;
+    }
+}
