ed8864788a6b698a69745eb404453f144985d528
CesarDaRocha <cesartapuio@gmail.com>
2023-06-02 20:45:57 -0300

    t08

---
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Grupo.java
@@ -0,0 +1,14 @@
+package com.github.cesardarocha.poo.t08;
+
+public class Grupo {
+    
+    
+        private Permissao permissao;
+        
+            public void setPermissao(Permissao permissao) {
+                this.permissao = permissao;
+            }
+        
+
+    
+}
