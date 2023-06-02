783545bb4cb3631842594b915e5fad741ff205b0
CesarDaRocha <cesartapuio@gmail.com>
2023-06-02 20:43:15 -0300

    t08

--- t08/src/main/java/com/github/cesardarocha/poo/t08/Permissao.java
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Permissao.java
@@ -1,17 +1,15 @@
 package com.github.cesardarocha.poo.t08;
 
 public class Permissao {
-//cidade = permissao
-//pais = arquivo
+
 
 private Arquivo arquivo;
 
-    // Construtor da Cidade
 
     public Permissao() {
     }
 
-    // Métodos de acesso para o País
+   
 
     public Arquivo getArquivo() {
         return arquivo;
