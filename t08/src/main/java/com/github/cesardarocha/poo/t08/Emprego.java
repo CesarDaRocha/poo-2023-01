3e9ef7eb9e3efc7b6560d56d7be0486ecfa44b18
CesarDaRocha <cesartapuio@gmail.com>
2023-06-02 21:24:22 -0300

    t08

--- t08/src/main/java/com/github/cesardarocha/poo/t08/Emprego.java
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Emprego.java
@@ -5,17 +5,24 @@ public class Emprego {
     private String inicio;
     private String fim;
     private Pessoa pessoa;
-
+    private Empresa empresa;
 
     public Emprego() {
     }
+    
 
     public Pessoa getPessoa() {
         return pessoa;
     }
+    public Empresa getEmpresa(){
+        return empresa;
+    }
 
     public void setPessoa(Pessoa pessoa) {
         this.pessoa = pessoa;
+    }
+    public void setEmpresa(Empresa empresa){
+        this.empresa = empresa;
     }
 
 }
