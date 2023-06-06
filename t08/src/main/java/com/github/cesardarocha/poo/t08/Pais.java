4ad678bb5feeca26ed3e3568a5297f2e076e9bc1
CesarDaRocha <cesartapuio@gmail.com>
2023-06-05 21:12:04 -0300

    t08

--- t08/src/main/java/com/github/cesardarocha/poo/t08/Pais.java
+++ t08/src/main/java/com/github/cesardarocha/poo/t08/Pais.java
@@ -4,8 +4,6 @@ package com.github.cesardarocha.poo.t08;
 public class Pais {
     private Cidade cidade;
 
-    public void setCidade(Cidade cidade) {
-        this.cidade = cidade;
-    }
+    
 }
 
