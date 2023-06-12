2756c8ede21861df7649b40fc5e9653a52108091
CesarDaRocha <cesartapuio@gmail.com>
2023-06-12 20:43:51 -0300

    t09

---
+++ t09/src/main/java/com/github/cesardarocha/poo/t09/Circulo.java
@@ -0,0 +1,13 @@
+package com.github.cesardarocha.poo.t09;
+
+public class Circulo {
+    int x;
+    int y;
+    float raio;
+
+    int transladar(int dx, int dy){
+        return dy;
+
+    }
+    
+}
