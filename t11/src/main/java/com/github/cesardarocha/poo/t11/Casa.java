57649603c1be5f78ea576caba41cd4798812700a
CesarDaRocha <cesartapuio@gmail.com>
2023-06-13 22:31:46 -0300

    t11

---
+++ t11/src/main/java/com/github/cesardarocha/poo/t11/Casa.java
@@ -0,0 +1,10 @@
+package com.github.cesardarocha.poo.t11;
+
+public class Casa {
+    private Area area;
+    private Sala sala;
+    private Quarto quarto;
+    private Banheiro banheiro;
+    private Garagem garagem;
+    
+}
