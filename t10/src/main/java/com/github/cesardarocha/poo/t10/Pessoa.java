89de3b5012d585169a5c2dcc87aa96a8158cca66
CesarDaRocha <cesartapuio@gmail.com>
2023-06-13 22:08:48 -0300

    t10

--- t10/src/main/java/com/github/cesardarocha/poo/t10/Pessoa.java
+++ t10/src/main/java/com/github/cesardarocha/poo/t10/Pessoa.java
@@ -1,8 +1,13 @@
 package com.github.cesardarocha.poo.t10;
 
+import java.util.ArrayList;
+
 public class Pessoa {
     String nome;
     String roupa;
+
+    ArrayList<Bicicleta> bicicletas;
+
     Bicicleta bicicleta = new Bicicleta();
 
     void pedala(Bicicleta bicicleta){
