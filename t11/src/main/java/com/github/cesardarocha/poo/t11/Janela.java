28360f24230c5cd5ca5022d13b568d5293df0d1b
CesarDaRocha <cesartapuio@gmail.com>
2023-06-13 22:42:10 -0300

    t11

---
+++ t11/src/main/java/com/github/cesardarocha/poo/t11/Janela.java
@@ -0,0 +1,5 @@
+package com.github.cesardarocha.poo.t11;
+
+public class Janela {
+    public Vidro area;
+}
---
+++ t11/src/main/java/com/github/cesardarocha/poo/t11/JanelaVidro.java
@@ -0,0 +1,6 @@
+package com.github.cesardarocha.poo.t11;
+
+public class JanelaVidro extends Janela {
+    public float areaVidro;
+    
+}
