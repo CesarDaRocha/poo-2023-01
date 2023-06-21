37c1b6eeca9e6a0b9a6e95fd91a13524c822a198
CesarDaRocha <cesartapuio@gmail.com>
2023-06-21 19:53:39 -0300

    t11

--- t11/src/main/java/com/github/cesardarocha/poo/t11/Lanchonete.java
+++ t11/src/main/java/com/github/cesardarocha/poo/t11/Lanchonete.java
@@ -3,4 +3,17 @@ package com.github.cesardarocha.poo.t11;
 public class Lanchonete {
     public String nome;
     public String gerenciador;
+    public Funcionario funcionarios[];
+    public String nomeFuncionario[];
+
+    String[] emprega(String nomeFuncionario[], Funcionario funcionario, int posicao){
+
+        nomeFuncionario[posicao] = funcionarios[posicao].nome;
+         
+        return nomeFuncionario;
+
+    }
+
+
+
 }
