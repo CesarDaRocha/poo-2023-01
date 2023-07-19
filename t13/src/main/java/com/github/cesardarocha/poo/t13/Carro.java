7749d343c8eaab229b4187e63b0ba4ef083dffbf
CesarDaRocha <cesartapuio@gmail.com>
2023-07-19 19:27:57 -0300

    t13

---
+++ t13/src/main/java/com/github/cesardarocha/poo/t13/Carro.java
@@ -0,0 +1,11 @@
+package com.github.cesardarocha.poo.t13;
+
+public class Carro {
+    String carro;
+    
+    public static void carr(String[] args){
+    Carro myCar1 = new Carro();
+    
+    System.out.println(myCar1);
+    }
+}
